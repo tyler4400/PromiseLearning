9d99c5f69f6e49571eefba9da33e51636500fbad
tyler4400 <tyler4400@foxmail.com>
2019-09-16 19:12:21 +0800

    3-1 then 的嵌套 done

---
+++ 3-1 then 的嵌套.js
@@ -0,0 +1,30 @@
+console.log('>>>>>>>started');
+
+new Promise(resolve => {
+    console.log('Step 1');
+    setTimeout(() => {
+        resolve('第一个promise');
+    }, 1000);
+})
+    .then(value => {
+        return new Promise(resolve => {
+            console.log('Step 1-1');
+            setTimeout(() => {
+                resolve('第二个promise');
+            }, 1000);
+        })
+            .then(value => {
+                console.log('Step 1-2');
+                return value;
+            })
+            .then(value => {
+                console.log('Step 1-3');
+                return value;
+            });
+    })
+    .then(value => {
+        console.log(value); // 第二个promise
+        console.log('Step 2');
+    });
+
+console.log('<<<<<<<ended');
